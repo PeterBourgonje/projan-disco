8ea446f824b6d73e4c81e9fbee0bb5a3e6ed543f
PeterBourgonje <bourgonje@gmail.com>
2023-08-09 13:24:36 +0200

    rudimentary projection working, needs further testing

--- project.py
+++ project.py
@@ -3,7 +3,19 @@ import align
 import discoparse
 import json
 import spacy
+from operator import itemgetter
+from itertools import groupby
 
+def is_continuous(lst): 
+    return all(a+1==b for a, b in zip(lst, lst[1:]))
+
+def get_sequences(lst):
+    seqs = []
+    for k,g in groupby(enumerate(lst),lambda x:x[0]-x[1]):
+        group = (map(itemgetter(1),g))
+        group = list(map(int,group))
+        seqs.append((group[0],group[-1]))
+    return seqs
 
 class ProjanDisco:
 
@@ -31,34 +43,44 @@ class ProjanDisco:
         trans_t2o = {t.i: (t.idx, t.idx+len(t.text)) for t in trans_tokenized}
         inp_o2t = {(t.idx, t.idx+len(t.text)): t.i for t in inp_tokenized}
         trans_o2t = {(t.idx, t.idx+len(t.text)): t.i for t in trans_tokenized}
-        print('deb alignments:', alignments)
         
         projected_relations = []
         if 'relations' in parse:
             relations = parse['relations']
             for relation in relations:
-                print('debrel:', relation)
                 prel = {}
                 for elem in relation:
-                    print('\tdeb elem:', elem)
-                    print('\t\tdeb cont:', relation[elem])
                     if isinstance(relation[elem], dict) and 'CharacterSpanList' in relation[elem] and 'TokenList' in relation[elem]:
                         projected = {'CharacterSpanList': [], 'RawText': '', 'TokenList': []}
                         src_tokens = []
                         # not guaranteed that tokenization is identical between alignment and discopy, so have to go by char offsets
-                        al_tokens = []
+                        alt_tokens = []
                         for charspan in relation[elem]['CharacterSpanList']:
                             s, e = charspan[0], charspan[1]
                             for span in trans_o2t:
                                 if s <= span[0] and span[1] <= e:
-                                    al_tokens.append(trans_o2t[span])
-                        print('deb altokens:', al_tokens)
-                        # TODO: have the aligned tokens here in al_tokens. Now use alignments to get the tokens on the pidgin/src text side, then figure out if that is a discontinuous array, then get charspanlists from there, and then it should be round-tripped.
+                                    alt_tokens.append(trans_o2t[span])
+                        aligned_tokens = [[a[0] for a in alignments if a[1] == t] for t in alt_tokens]
+                        aligned_tokens = sorted(list(set([t for tl in aligned_tokens for t in tl])))
+                        if aligned_tokens:
+                            projected['TokenList'] = aligned_tokens
+                            if is_continuous(aligned_tokens):
+                                s, e = inp_t2o[aligned_tokens[0]][0], inp_t2o[aligned_tokens[-1]][1]
+                                projected['CharacterSpanList'] = [[s, e]]
+                                projected['RawText'] = inp[s:e]
+                            else:
+                                seqs = get_sequences(aligned_tokens)
+                                for seq in seqs:
+                                    s, e = inp_t2o[seq[0]][0], inp_t2o[seq[-1]][1]
+                                    projected['CharacterSpanList'].append([s, e])
+                                    projected['RawText'] += ' ' + inp[s:e]
+                                projected['RawText'] = projected['RawText'].strip()
                         prel[elem] = projected
                     else:
                         prel[elem] = relation[elem]
                 projected_relations.append(prel)
-        print('Projected relations:', projected_relations)
+        
+        return projected_relations
                     
 def main():
     # TODO: wrap annotate() in sentence loop
