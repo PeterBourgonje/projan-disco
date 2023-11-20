dd5c6dc8fbc6918bc70f9b6e47ebf1f60c1d9b2c
PeterBourgonje <bourgonje@gmail.com>
2023-11-20 15:02:32 +0100

    implemented batch-wise processing to avoid memory issues

--- discoparse.py
+++ discoparse.py
@@ -8,6 +8,15 @@ docker pull rknaebel/discopy:1.0.2
 docker run -it --rm  -p 8080:8080 rknaebel/discopy:1.0.2
 """
 
+DISCOPY_BATCH_SIZE_SENTENCES = 300
+
+
+def batch(inp, batch_size):
+    lst = len(inp)
+    for ndx in range(0, lst, batch_size):
+        yield inp[ndx:min(ndx + batch_size, lst)]
+
+
 class Discopy:
 
     def __init__(self):
@@ -26,10 +35,41 @@ class Discopy:
             sys.exit()
 
     def parse(self, sentences):
-        data = json.dumps({'sentences': sentences})
+        final_response = {'docID': None,
+                          'meta': None,
+                          'text': '',
+                          'sentences': [],
+                          'relations': []
+                          }
         try:
-            response = requests.request("POST", self.url, headers=self.headers, data=data)
-            return response.text
+            # ran out of memory for longer texts, so doing batch-wise parsing here. This is very rudimentary, and just
+            # cuts up the sentences into batches as if it were separate texts. TODO: implement overlapping batches..
+            # Implies dealing with merging potentially conflicting information for the same input.
+            char_offset = 0
+            token_offset = 0
+            for b in batch(sentences, DISCOPY_BATCH_SIZE_SENTENCES):
+                data = json.dumps({'sentences': b})
+                response = requests.request("POST", self.url, headers=self.headers, data=data)
+                jr = json.loads(response.text)
+                final_response['docID'] = jr['docID']
+                final_response['meta'] = jr['meta']
+                final_response['text'] += jr['text']
+                final_response['sentences'] += jr['sentences']
+                relations = jr['relations']
+                for rel in relations:
+                    for elem in rel:
+                        if isinstance(rel[elem], dict) and 'TokenList' in rel[elem]:
+                            rel[elem]['TokenList'] = [x + token_offset for x in rel[elem]['TokenList']]
+                            cpl = rel[elem]['CharacterSpanList']
+                            for i, span in enumerate(cpl):
+                                span = [x + char_offset for x in span]
+                                cpl[i] = span
+                            # TODO: There seems to be some weird stuff going on, with r2l character spans like [71, 5]
+                char_offset += len(jr['text'])
+                token_offset += sum(len(x) for x in b)
+                final_response['relations'] += relations
+            assert len(sentences) == len(final_response['sentences'])
+            return json.dumps(final_response)
         except Exception as e:
             return str(e)
         
